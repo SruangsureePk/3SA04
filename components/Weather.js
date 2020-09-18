9327fb0d09e9972da9c464e76bd5f5ed5ffffb25
SruangsureePk <sruangsuree.paksa@gmail.com>
2020-09-18 11:55:54 +0700

    commitรอบที่เท่าไหร่เเล้วก็ไม่รู้:clown_face:

--- components/Weather.js
+++ components/Weather.js
@@ -12,9 +12,10 @@ export default function Weather(props) {
         <View style={styles.size}>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
                 <View style={styles.box}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
-                <Forecast {...forecastInfo} />
+                    <Text style={styles.big}>Zip Code</Text>
+                    <Text style={styles.center}>Zip Code</Text>
+                    <Text>{props.zipCode}</Text>
+                    <Forecast {...forecastInfo} />
                 </View>
             </ImageBackground>
         </View>
@@ -28,11 +29,21 @@ const styles = StyleSheet.create({
     },
     size: {
         width: '100%',
-    }
-    box:{
+    },
+    box: {
         backgroundColor: '#000',
         width: '100%',
         height: '40%',
         opacity: 0.5,
+        alignItems: 'center',
+        paddingTop: 40,
+    },
+    big: {
+        fontSize: 30,
+        color: 'white'
+    },
+    center: {
+        alignItems: 'center'
     }
+
 });
