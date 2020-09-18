dcc21ecb4588e54791a4321b40f7c8bfc2401bfc
SruangsureePk <sruangsuree.paksa@gmail.com>
2020-09-18 11:23:24 +0700

    commitรอบที่2นะจ้ะ:green_heart:

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, CheckBox } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
@@ -11,9 +11,11 @@ export default function Weather(props) {
     return (
         <View style={styles.size}>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.box}>
                 <Text>Zip Code</Text>
                 <Text>{props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
+                </View>
             </ImageBackground>
         </View>
     );
@@ -26,5 +28,11 @@ const styles = StyleSheet.create({
     },
     size: {
         width: '100%',
+    }
+    box:{
+        backgroundColor: '#000',
+        width: '100%',
+        height: '40%',
+        opacity: 0.5,
     }
 });
