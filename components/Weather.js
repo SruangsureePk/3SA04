bf1a4f317a368d598b6cea99810d869ab85b990e
SruangsureePk <sruangsuree.paksa@gmail.com>
2020-09-25 10:21:03 +0700

    commit6อันจุกๆ

--- components/Weather.js
+++ components/Weather.js
@@ -1,20 +1,43 @@
-import React, { useState } from 'react';
-import { View, Text, ImageBackground, StyleSheet, CheckBox } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
+
+    const apiKey = '6cecb9dd2e369d7e9b5d62bc682150d4'
+
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
-        temp: 0
+        main: 'main',
+        description: 'description',
+        temp: 0,
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {   
+
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
+
+
     return (
-        <View style={styles.size}>
+        <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <View style={styles.box}>
-                    <Text style={styles.big}>Zip Code</Text>
-                    <Text style={styles.center}>Zip Code</Text>
-                    <Text>{props.zipCode}</Text>
+                <View style={styles.cover}>
+                    <Text style={styles.medium}>Zip Code: {props.zipCode}</Text>
                     <Forecast {...forecastInfo} />
                 </View>
             </ImageBackground>
@@ -25,25 +48,18 @@ const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
         width: '100%',
-        height: '100%'
+        height: '100%',
     },
-    size: {
-        width: '100%',
-    },
-    box: {
+    cover: {
         backgroundColor: '#000',
         width: '100%',
-        height: '40%',
+        height: 300,
         opacity: 0.5,
+        justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: 40,
     },
-    big: {
-        fontSize: 30,
-        color: 'white'
-    },
-    center: {
-        alignItems: 'center'
+    medium: {
+        fontSize: 20,
+        color: '#FFF',
     }
-
 });
